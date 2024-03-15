e4e79d73d005862d1fa2c13b4991f0fc4932292c
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-03-15 11:25:46 +0000

    added filter at the top of stocks page

--- components/Stocks/StockList/index.tsx
+++ components/Stocks/StockList/index.tsx
@@ -1,4 +1,13 @@
 import { Input } from '@/components/ui/input';
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 
 const StockList = () => {
 	return (
@@ -9,6 +18,20 @@ const StockList = () => {
 						className='text-black border-black'
 						placeholder='Search item by name...'
 					/>
+					<Select>
+						<SelectTrigger className='w-[180px] text-black border-black'>
+							<SelectValue placeholder='Stock Status' />
+						</SelectTrigger>
+						<SelectContent className='text-black border-black'>
+							<SelectGroup>
+								<SelectItem value='show-all'>Show All</SelectItem>
+								<SelectItem value='in-stock'>In Stock</SelectItem>
+								<SelectItem value='out-of-stock'>
+									Out of Stock
+								</SelectItem>
+							</SelectGroup>
+						</SelectContent>
+					</Select>
 				</div>
 			</div>
 		</>
