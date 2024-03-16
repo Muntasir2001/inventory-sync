d3b46c280ad37ba7f9c055042521ea1961d48193
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-03-16 16:40:40 +0000

    updated UI of the stock

--- components/Stocks/StockList/index.tsx
+++ components/Stocks/StockList/index.tsx
@@ -38,13 +38,13 @@ const StockList = () => {
 						</SelectContent>
 					</Select>
 				</div>
-				<div className='flex flex-col gap-3'>
+				<div className='flex flex-col gap-6'>
 					<div className='flex gap-8'>
 						<div className='bg-dark-white rounded-md px-5 flex items-center justify-center max-h-[96px]'>
 							<i className='ri-box-3-fill text-black text-5xl' />
 						</div>
 						<div className='flex flex-col gap-2'>
-							<h3 className='text-2xl text-black font-medium'>
+							<h3 className='text-2xl text-black font-medium max-w-[130px]'>
 								Abaya long
 							</h3>
 							<p className='text-black'>Code : AB-123</p>
@@ -57,10 +57,38 @@ const StockList = () => {
 								<i className='ri-menu-line text-black text-3xl' />
 							</DropdownMenuTrigger>
 							<DropdownMenuContent className='text-black mr-4'>
-								<DropdownMenuItem className='font-medium'>
+								<DropdownMenuItem className='font-medium text-xl active:bg-dark-white'>
 									Edit
 								</DropdownMenuItem>
-								<DropdownMenuItem className='text-red font-medium'>
+								<DropdownMenuItem className='text-red font-medium text-xl active:bg-dark-white'>
+									Delete
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+
+					<div className='flex gap-8'>
+						<div className='bg-dark-white rounded-md px-5 flex items-center justify-center max-h-[96px]'>
+							<i className='ri-box-3-fill text-black text-5xl' />
+						</div>
+						<div className='flex flex-col gap-2'>
+							<h3 className='text-2xl text-black font-medium max-w-[150px] text-ellipsis whitespace-nowrap overflow-hidden'>
+								Abaya longggggggg
+							</h3>
+							<p className='text-black'>Code : AB-123</p>
+							<p className='text-black'>
+								Status : <span className='text-green'>In Stock</span>
+							</p>
+						</div>
+						<DropdownMenu>
+							<DropdownMenuTrigger className='ml-auto outline-none'>
+								<i className='ri-menu-line text-black text-3xl' />
+							</DropdownMenuTrigger>
+							<DropdownMenuContent className='text-black mr-4'>
+								<DropdownMenuItem className='font-medium text-xl active:bg-dark-white'>
+									Edit
+								</DropdownMenuItem>
+								<DropdownMenuItem className='text-red font-medium text-xl active:bg-dark-white'>
 									Delete
 								</DropdownMenuItem>
 							</DropdownMenuContent>
