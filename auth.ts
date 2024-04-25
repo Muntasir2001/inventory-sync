a2b1c093f62b86aded1080cec60875a586365251
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-04-25 22:49:58 +0100

    working on nextauth

--- auth.ts
+++ auth.ts
@@ -1,18 +1,8 @@
 import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [
-		Credentials({
-			credentials: {
-				email: { label: 'email' },
-				password: { label: 'Password', type: 'password' },
-			},
-			async authorize({ request }: any) {
-				const response = await fetch(request);
-				if (!response.ok) return null;
-				return (await response.json()) ?? null;
-			},
-		}),
-	],
+export const { auth, signIn, signOut } = NextAuth({
+	...authConfig,
+	providers: [Credentials({})],
 });
