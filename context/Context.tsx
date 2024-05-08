b459a30a87f7c6303357f961e4bb0cf83c757e69
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-05-08 06:54:12 +0100

    working on prisma functions

--- context/Context.tsx
+++ context/Context.tsx
@@ -5,7 +5,6 @@ import {
 	useContext,
 	useMemo,
 	useReducer,
-	FC,
 	Dispatch,
 } from 'react';
 
--- prisma/functions/currencies.ts
+++ prisma/functions/currencies.ts
@@ -1,7 +1,15 @@
 'use server';
 
-import { Prisma } from '@prisma/client';
+import { prisma } from '../prisma';
 
 interface GetCurrency {
 	id: number;
 }
+
+export const GetCurrency = async () => {};
+
+export const GetAllCurrencies = async () => {
+	const data = await prisma.currencies.findMany();
+
+	return data;
+};
