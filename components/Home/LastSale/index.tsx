645b26a8412568a0579eb7712c472d2c3556da78
Muntasir2001 <muntasirenamalhuda@gmail.com>
2024-09-07 16:55:31 +0100

    working on latest sales section in homepage

--- components/Home/LastSale/index.tsx
+++ components/Home/LastSale/index.tsx
@@ -1,6 +1,29 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
 import SaleCard from './SaleCard';
+import { getAllSales } from '@/redux/sale/saleSlice';
+import { selectUser } from '@/redux/user/selectors';
+import { selectSales } from '@/redux/sale/selectors';
+import { useAppSelector } from '@/redux/store';
+import { useAppDispatch } from '@/redux/store';
 
 const LastSale = async () => {
+	const [loading, setLoading] = useState<boolean>(true);
+
+	const dispatch = useAppDispatch();
+	const sales = useAppSelector(selectSales);
+	const user = useAppSelector(selectUser);
+
+	useEffect(() => {
+		if (user && sales.length < 1) {
+			dispatch(getAllSales(user.id));
+		}
+
+		setLoading(false);
+	}, [user, sales.length]);
+
 	return (
 		<>
 			<div className='flex flex-col gap-4 mt-10 pb-6 px-5'>
